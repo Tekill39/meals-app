2c0370b40ab1ac6e63264477c382a89b4affaa31
Tekill39 <minit009@yandex.ru>
2021-06-06 14:47:07 +0200

    COLORS AND navigationOptions CategoriesScreen.

--- components/screens/CategoriesScreen.js
+++ components/screens/CategoriesScreen.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import {View, Text, StyleSheet, Flatlist} from 'react-native';
-import {CATEGORIES} from '../../data/dummy-data';
 
-const renderGridItem = (itemData) => {
-    return( 
-        <View style={styles.gridItem}>
-            <Text>{itemData.item.title}</Text>
-        </View>)
-    }
-const CategoriesScreen = props => {    
-    
-  return(
-      <Flatlist
-      keyExtractor = {(item, index)=>item.id}  
-      data={CATEGORIES} 
-      renderItem={renderGridItem} 
-      numColumns={2}/>
+
+import React from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+
+import {CATEGORIES} from '../../data/dummy-data';
+import COLOR from '../../constant/Colors';
+
+
+const CategoriesScreen = props => {
+  const renderGridItem = itemData => {   
+  return (
+    <TouchableOpacity style={styles.gridItem} onPress={()=>{
+        props.navigation.navigate({routeName:'CategoryMeals'})
+    }}>
+      <View >
+        <Text>{itemData.item.title}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
- const styles = StyleSheet.create({
-   gridItem: {
+
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
+};
+CategoriesScreen.navigationOptions={
+    headerTitle:"Meal Categories",
+    headerStyle:{
+        backgroundColor:COLOR.primaryColor
+    }
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  gridItem: {
     flex: 1,
     margin: 15,
     height: 150
