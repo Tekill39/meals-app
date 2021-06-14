c50a725823e15cc7bf4b2e96285dc11c37492b76
Tekill39 <minit009@yandex.ru>
2021-06-14 14:12:59 +0200

    Meals list

--- components/screens/CategoryMealScreen.js
+++ components/screens/CategoryMealScreen.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 import {CATEGORIES, MEALS} from '../../data/dummy-data';
+import MealItem from '../MealItem';
 
 const CategoryMealScreen = props => {
     const renderMealItem = itemData => {
-        return(<View><Text>{itemData.item.title}</Text></View>);
+        return(
+          <MealItem 
+          title={itemData.item.title}
+          image={itemData.item.imageUrl}
+          duration ={itemData.item.duration}
+          complexity={itemData.item.complexity}
+          affordability={itemData.item.affordability} 
+          onSelectMeal={()=>{
+
+          }}/>
+        );
     }
     const catId = props.navigation.getParam('categoryId');
 
