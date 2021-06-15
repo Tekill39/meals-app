d183145d52055dd67113466ef8d6993466852233
Tekill39 <minit009@yandex.ru>
2021-06-15 07:48:47 +0200

    Adding Header Buttons

--- components/screens/MealDetailScreen.js
+++ components/screens/MealDetailScreen.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
+import {MEALS} from '../../data/dummy-data';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import HeaderButton from '../HeaderButtons';
 
 const MealDetailScreen = props => {
+    const mealId = props.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal=> meal.id === mealId);
   return(
         <View style = {styles.screen}>
-           <Text> The Meal Detail Screen!</Text>
+           <Text> {selectedMeal.title}</Text>
            <Button title="Go back to categores" onPress={()=>{
                props.navigation.popToTop();
            }}/>  
         </View>);
 };
-
+MealDetailScreen.navigationOptions = (navigationData)=> {
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal=> meal.id === mealId);
+    return{
+        headerTitle:selectedMeal.title ,
+        headerRight:(<HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item 
+            title="Favorite" 
+            iconName='ios-star'
+            onPress={()=> {
+                console.log('Press')}
+                }/>
+        </HeaderButtons>)
+    };
+}
 const styles = StyleSheet.create({
     screen:{
         flex:1,
