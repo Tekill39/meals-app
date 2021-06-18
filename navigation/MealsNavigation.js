4d7f9aed2af305b95cacf06fe3ca42da21fefeab
Tekill39 <minit009@yandex.ru>
2021-06-18 12:23:19 +0200

    Setting Icons

--- navigation/MealsNavigation.js
+++ navigation/MealsNavigation.js
@@ -1,4 +1,5 @@
 import { Platform } from 'react-native';
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator} from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
@@ -7,6 +8,7 @@ import CategoryMealScreen from '../components/screens/CategoryMealScreen';
 import MealDetailScreen from '../components/screens/MealDetailScreen';
 import COLOR from '../constant/Colors';
 import FavoritesScreen from '../components/screens/FavoritesScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const MealsNavigator = createStackNavigator({
   Categories:CategoriesScreen,
@@ -24,8 +26,35 @@ const MealsNavigator = createStackNavigator({
 });
 
 const MealsFavTabNavigator = createBottomTabNavigator({
-Meals:MealsNavigator,
-Favorites:FavoritesScreen
+Meals:{screen:MealsNavigator, navigationOptions:{
+  tabBarIcon:(tabInfo) => {
+    return<Ionicons 
+      name="ios-restaurant" 
+      size={25}
+      color={tabInfo.tintColor}/>
+  }
+}},
+Favorites:{
+  screen:FavoritesScreen,
+  navigationOptions:{
+  tabBarlabel:'Favorites',  
+  tabBarIcon:(tabInfo) => {
+    return<Ionicons 
+      name="ios-star" 
+      size={25}
+      color={tabInfo.tintColor}/>
+  }
+}}
+},{
+tabBarOptions: {
+  activeTintColor: COLOR.accentColor,
+  labelStyle: {
+    fontSize: 14,
+  },
+  // style: {
+  //   backgroundColor: 'blue',
+  // },
+}
 });
 
 export default createAppContainer(MealsFavTabNavigator);
